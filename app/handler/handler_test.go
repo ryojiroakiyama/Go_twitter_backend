f0a509fdaf0df9cf718d8b0cb7e9947f67140618
rakiyama <ryojiro25@gmail.com>
2022-03-29 12:00:34 +0900

    add: test: base by make expect data

--- app/handler/handler_test.go
+++ app/handler/handler_test.go
@@ -4,7 +4,6 @@ import (
 	"bytes"
 	"context"
 	"encoding/json"
-	"fmt"
 	"io"
 	"net/http"
 	"net/http/httptest"
@@ -13,13 +12,19 @@ import (
 	"testing"
 
 	"yatter-backend-go/app/app"
+	"yatter-backend-go/app/dao"
 	"yatter-backend-go/app/domain/object"
+	"yatter-backend-go/app/domain/repository"
 
 	"github.com/stretchr/testify/assert"
 )
 
 func TestAccountRegistration(t *testing.T) {
-	c := setup(t)
+	expect := &mockDao{}
+	expect.account.findbyusername.obj = &object.Account{
+		Username: "john",
+	}
+	c := setup(t, expect)
 	defer c.Close()
 
 	func() {
@@ -63,11 +68,8 @@ func TestAccountRegistration(t *testing.T) {
 	}()
 }
 
-func setup(t *testing.T) *C {
-	app, err := app.NewApp()
-	if err != nil {
-		panic(err)
-	}
+func setup(t *testing.T, d dao.Dao) *C {
+	app := &app.App{Dao: d}
 
 	if err := app.Dao.InitAll(); err != nil {
 		t.Fatal(err)
@@ -81,32 +83,88 @@ func setup(t *testing.T) *C {
 	}
 }
 
-// mapは重複してたら上書きしてしまうので, insert前に確認忘れずに
-type account struct {
-	account map[string]*object.Account
+type retFindBy struct {
+	obj interface{}
+	err error
 }
 
-func (r *account) FindByUsername(ctx context.Context, username string) (*object.Account, error) {
-	account, exist := r.account[username]
-	if exist {
-		return account, nil
+type retCreate struct {
+	id  int64
+	err error
+}
+
+type retDelete struct {
+	err error
+}
+
+type retAll struct {
+	obj interface{}
+	err error
+}
+
+type mockAccount struct {
+	findbyusername retFindBy
+	create         retCreate
+}
+
+func (r *mockAccount) FindByUsername(ctx context.Context, username string) (*object.Account, error) {
+	obj, ok := r.findbyusername.obj.(*object.Account)
+	if !ok {
+		panic("findbyuser")
 	}
-	return nil, fmt.Errorf("FindByUsername: not exist")
+	return obj, r.findbyusername.err
 }
 
-// Create: アカウント作成
-func (r *account) Create(ctx context.Context, entity *object.Account) (object.AccountID, error) {
-	_, exist := r.account[entity.Username]
-	if exist {
-		return 0, fmt.Errorf("Create: aready exist")
+func (r *mockAccount) Create(ctx context.Context, entity *object.Account) (object.AccountID, error) {
+	return r.create.id, r.create.err
+}
+
+type mockStatus struct {
+	findbyid retFindBy
+	create   retCreate
+	delete   retDelete
+	all      retAll
+}
+
+func (r *mockStatus) FindByID(ctx context.Context, id object.StatusID) (*object.Status, error) {
+	obj, ok := r.findbyid.obj.(*object.Status)
+	if !ok {
+		panic("findbyid")
 	}
-	id := len(r.account) + 1
-	r.account[entity.Username] = entity
-	return int64(id), nil
+	return obj, r.findbyid.err
 }
 
-type status struct {
-	status map[object.StatusID]*object.Status
+func (r *mockStatus) Create(ctx context.Context, entity *object.Status) (object.AccountID, error) {
+	return r.create.id, r.create.err
+}
+
+func (r *mockStatus) Delete(ctx context.Context, status_id object.StatusID, account_id object.AccountID) error {
+	return r.delete.err
+}
+
+func (r *mockStatus) All(ctx context.Context) ([]object.Status, error) {
+	obj, ok := r.all.obj.([]object.Status)
+	if !ok {
+		panic("all")
+	}
+	return obj, r.all.err
+}
+
+type mockDao struct {
+	account mockAccount
+	status  mockStatus
+}
+
+func (d *mockDao) Account() repository.Account {
+	return &d.account
+}
+
+func (d *mockDao) Status() repository.Status {
+	return &d.status
+}
+
+func (d *mockDao) InitAll() error {
+	return nil
 }
 
 type C struct {

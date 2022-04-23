474b443a3e7890d9cfdaa5300010f80ae5946a28
rakiyama <ryojiro25@gmail.com>
2022-04-23 17:15:07 +0900

    viewを修正

--- ddl/view.sql
+++ ddl/view.sql
@@ -1,26 +1,28 @@
 CREATE VIEW meta_account
 AS
   SELECT
+    a.id,
     a.username,
+    a.password_hash,
     a.display_name,
     a.avatar,
     a.header,
     a.note,
     a.create_at,
-    r_user.cnt_user_id_rows AS "following_count",
-    r_follow.cnt_follow_id_rows AS "followers_count"
+    r_group_by_user.cnt AS "following_count",
+    r_group_by_follow.cnt AS "followers_count"
   FROM account AS a
     INNER JOIN
       (SELECT
         user_id,
-        COUNT(*) AS cnt_user_id_rows
+        COUNT(*) AS cnt
       FROM relationship
-      GROUP BY user_id) AS r_user
-    ON a.id = r_user.user_id
+      GROUP BY user_id) AS r_group_by_user
+    ON a.id = r_group_by_user.user_id
     INNER JOIN
       (SELECT
         follow_id,
-        COUNT(*) AS cnt_follow_id_rows
+        COUNT(*) AS cnt
       FROM relationship
-      GROUP BY follow_id) AS r_follow
-    ON a.id = r_follow.follow_id;
+      GROUP BY follow_id) AS r_group_by_follow
+    ON a.id = r_group_by_follow.follow_id;
--- into_mysql.sh
+++ into_mysql.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 echo "tips"
-echo "mysql -u yatter -pyatter"
 echo "mysql> use yatter;"
 echo "mysql> show tables;"
 echo "mysql> desc account;"
@@ -9,4 +8,5 @@ echo "mysql> show databases;"
 echo "mysql> select * from account;"
 
 set -eux
+echo "mysql -u yatter -pyatter" | pbcopy # コンテナに入った後, コピペでmysqlまで入れるように
 docker-compose exec mysql /bin/bash

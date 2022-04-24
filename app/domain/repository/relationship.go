7fe4da3e78b0138c9951eea69a38cfb10c6e9fff
rakiyama <ryojiro25@gmail.com>
2022-04-24 13:40:10 +0900

    NumOfFollowingメソッド削除

--- app/domain/repository/relationship.go
+++ app/domain/repository/relationship.go
@@ -24,7 +24,4 @@ type Relationship interface {
 
 	// Delete Relationship
 	Delete(ctx context.Context, userID object.AccountID, targetID object.AccountID) error
-
-	// Get number of following accounts
-	NumberOfFollowingAccounts(ctx context.Context, username string) (int64, error)
 }

c56cc9aa18129d8b2c617946891c5a2bfb594cd5
rakiyama <ryojiro25@gmail.com>
2022-05-01 13:10:40 +0900

    mysqlもtestmysqlもddlは共有

--- enter_mysql.sh
+++ enter_mysql.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-echo "tips"
+echo "next flow"
+echo "mysql -uyatter -pyatter"
 echo "mysql> use yatter;"
 echo "mysql> show tables;"
 echo "mysql> desc account;"
@@ -8,5 +9,4 @@ echo "mysql> show databases;"
 echo "mysql> select * from account;"
 
 set -eux
-echo "mysql -u yatter -pyatter" | pbcopy # コンテナに入った後, コピペでmysqlまで入れるように
 docker-compose exec mysql /bin/bash
--- init_db.sh
+++ init_db.sh
@@ -2,4 +2,4 @@
 
 set -eux
 
-rm -rfd .data/mysql
+rm -rfd .data/mysql .data/testmysql

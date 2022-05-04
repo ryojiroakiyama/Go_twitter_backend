8a2ed57cd4b6648c1c0ae6897cf3b740bf707b64
rakiyama <ryojiro25@gmail.com>
2022-05-04 20:38:39 +0900

    accountfindbyusenameテスト追加

--- app/dao/account_test.go
+++ app/dao/account_test.go
@@ -8,9 +8,51 @@ import (
 	_ "github.com/go-sql-driver/mysql"
 )
 
-func TestAccount(t *testing.T) {
+const (
+	testuser = "benben"
+)
+
+func TestAccountFindByUsername(t *testing.T) {
+	// set up
 	dao := NewDao(t)
-	a := object.Account{Username: "test"}
-	dao.Account().Create(context.Background(), &a)
 	defer dao.InitAll()
+	ctx := context.Background()
+	account := object.Account{Username: testuser}
+	dao.Account().Create(ctx, &account)
+
+	type args struct {
+		ctx      context.Context
+		username string
+	}
+	tests := []struct {
+		name    string
+		args    args
+		want    *object.Account
+		wantErr bool
+	}{
+		{
+			name: "simple",
+			args: args{
+				ctx:      ctx,
+				username: testuser,
+			},
+			want: &object.Account{
+				Username: testuser,
+			},
+			wantErr: false,
+		},
+	}
+	for _, tt := range tests {
+		tt := tt
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := dao.Account().FindByUsername(ctx, tt.args.username)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("account.FindByUsername() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if got.Username != tt.want.Username {
+				t.Errorf("account.FindByUsername() = %v, want %v", got, tt.want)
+			}
+		})
+	}
 }

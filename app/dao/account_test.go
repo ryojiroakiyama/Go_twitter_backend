5e6c574483366e0906fafed7bcab683d485c25af
rakiyama <ryojiro25@gmail.com>
2022-05-04 15:46:51 +0900

    testphpmyadmin追加

--- app/dao/account_test.go
+++ app/dao/account_test.go
@@ -1,12 +1,16 @@
 package dao_test
 
 import (
+	"context"
 	"testing"
+	"yatter-backend-go/app/domain/object"
 
 	_ "github.com/go-sql-driver/mysql"
 )
 
 func TestAccount(t *testing.T) {
 	dao := NewDao()
+	a := object.Account{Username: "test"}
+	dao.Account().Create(context.Background(), &a)
 	defer Done(dao)
 }

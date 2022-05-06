0cccdfa9cee2b40457f4fc0594a561f8e95b2cba
rakiyama <ryojiro25@gmail.com>
2022-05-06 19:25:50 +0900

    add test: followers

--- app/dao/account_test.go
+++ app/dao/account_test.go
@@ -163,6 +163,20 @@ func TestAccountFollowing(t *testing.T) {
 			wantErr: false,
 		},
 		{
+			name: "limit",
+			args: args{
+				ctx:      ctx,
+				username: testUsername1,
+				limit:    1,
+			},
+			want: []object.Account{
+				{
+					Username: testUsername2,
+				},
+			},
+			wantErr: false,
+		},
+		{
 			name: "no account",
 			args: args{
 				ctx:      ctx,
@@ -181,10 +195,75 @@ func TestAccountFollowing(t *testing.T) {
 				t.Errorf("account.Following() error = %v, wantErr %v", err, tt.wantErr)
 				return
 			}
-			for i, v := range got {
-				if v.Username != tt.want[i].Username {
-					t.Errorf("account.Following() = %v, want %v", got, tt.want)
+			if len(got) == len(tt.want) {
+				for i, v := range got {
+					if v.Username != tt.want[i].Username {
+						t.Errorf("account.Following() = %v, want %v", got, tt.want)
+					}
 				}
+			} else {
+				t.Errorf("account.Following() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestAccountFollowers(t *testing.T) {
+	// set up
+	dao := NewDao(t)
+	defer dao.InitAll()
+	ctx := context.Background()
+	CreateBaseTable(t, dao)
+
+	type args struct {
+		ctx      context.Context
+		username string
+		since_id int64
+		max_id   int64
+		limit    int64
+	}
+	tests := []struct {
+		name    string
+		args    args
+		want    []object.Account
+		wantErr bool
+	}{
+		{
+			name: "simple",
+			args: args{
+				ctx:      ctx,
+				username: testUsername3,
+				since_id: 0,
+				max_id:   10,
+				limit:    10,
+			},
+			want: []object.Account{
+				{
+					Username: testUsername1,
+				},
+				{
+					Username: testUsername2,
+				},
+			},
+			wantErr: false,
+		},
+	}
+	for _, tt := range tests {
+		tt := tt
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := dao.Account().Followers(tt.args.ctx, tt.args.username, tt.args.since_id, tt.args.max_id, tt.args.limit)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("account.Followers() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if len(got) == len(tt.want) {
+				for i, v := range got {
+					if v.Username != tt.want[i].Username {
+						t.Errorf("account.Followers() = %v, want %v", got, tt.want)
+					}
+				}
+			} else {
+				t.Errorf("account.Followers() = %v, want %v", got, tt.want)
 			}
 		})
 	}

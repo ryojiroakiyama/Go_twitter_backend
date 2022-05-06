7a590fe6155e0f8ea42732b712912ade972a3279
rakiyama <ryojiro25@gmail.com>
2022-05-06 20:27:15 +0900

    apdate test: statusContent

--- app/dao/setup_test.go
+++ app/dao/setup_test.go
@@ -12,9 +12,12 @@ import (
 )
 
 const (
-	testUsername1 = "benben"
-	testUsername2 = "jonjon"
-	testUsername3 = "sonson"
+	testUsername1      = "benben"
+	testUsername2      = "jonjon"
+	testUsername3      = "sonson"
+	testStatusContent1 = "status of account1"
+	testStatusContent2 = "status of account2"
+	testStatusContent3 = "status of account3"
 )
 
 func NewDao(t *testing.T) dao.Dao {
@@ -52,9 +55,9 @@ func CreateBaseTable(t *testing.T, dao dao.Dao) {
 	account1.ID = mustAccountCreate(t, dao, ctx, &account1)
 	account2.ID = mustAccountCreate(t, dao, ctx, &account2)
 	account3.ID = mustAccountCreate(t, dao, ctx, &account3)
-	status1 := object.Status{Content: "status of account1", Account: &account1}
-	status2 := object.Status{Content: "status of account2", Account: &account2}
-	status3 := object.Status{Content: "status of account3", Account: &account3}
+	status1 := object.Status{Content: testStatusContent1, Account: &account1}
+	status2 := object.Status{Content: testStatusContent2, Account: &account2}
+	status3 := object.Status{Content: testStatusContent3, Account: &account3}
 	mustStatusCreate(t, dao, ctx, &status1)
 	mustStatusCreate(t, dao, ctx, &status2)
 	mustStatusCreate(t, dao, ctx, &status3)
--- app/dao/status_test.go
+++ app/dao/status_test.go
@@ -16,7 +16,7 @@ func TestStatus_FindByID_Delete(t *testing.T) {
 
 	if s, err := dao.Status().FindByID(ctx, 1); err != nil {
 		t.Fatalf("FindByID %v", err)
-	} else if s.Account.Username != testUsername1 {
+	} else if s.Content != testStatusContent1 {
 		t.Errorf("FindByID returned: %v", s)
 	}
 
@@ -30,3 +30,56 @@ func TestStatus_FindByID_Delete(t *testing.T) {
 		t.Errorf("FindByID returned: %v", s)
 	}
 }
+
+//func TestStatusAllStatuses(t *testing.T) {
+//	// set up
+//	dao := NewDao(t)
+//	defer dao.InitAll()
+//	ctx := context.Background()
+//	CreateBaseTable(t, dao)
+
+//	type args struct {
+//		ctx      context.Context
+//		since_id int64
+//		max_id   int64
+//		limit    int64
+//	}
+//	tests := []struct {
+//		name    string
+//		args    args
+//		want    []object.Status
+//		wantErr bool
+//	}{
+//		{
+//			name: "simple",
+//			args: args{
+//				ctx:      ctx,
+//				since_id: 0,
+//				max_id:   10,
+//				limit:    10,
+//			},
+//			want: []object.Status{
+//				{
+//					Content: "",
+//				},
+//			},
+//		},
+//	}
+//	for _, tt := range tests {
+//		tt := tt
+//		t.Run(tt.name, func(t *testing.T) {
+//			t.Parallel()
+//			r := &status{
+//				db: tt.fields.db,
+//			}
+//			got, err := r.AllStatuses(tt.args.ctx, tt.args.since_id, tt.args.max_id, tt.args.limit)
+//			if (err != nil) != tt.wantErr {
+//				t.Errorf("status.AllStatuses() error = %v, wantErr %v", err, tt.wantErr)
+//				return
+//			}
+//			if !reflect.DeepEqual(got, tt.want) {
+//				t.Errorf("status.AllStatuses() = %v, want %v", got, tt.want)
+//			}
+//		})
+//	}
+//}

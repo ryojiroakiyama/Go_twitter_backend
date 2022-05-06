805b779ea0397df180fcd3a378f653e689c94967
rakiyama <ryojiro25@gmail.com>
2022-05-06 20:33:46 +0900

    add test: AllStatuses

--- app/dao/status_test.go
+++ app/dao/status_test.go
@@ -3,6 +3,7 @@ package dao_test
 import (
 	"context"
 	"testing"
+	"yatter-backend-go/app/domain/object"
 
 	_ "github.com/go-sql-driver/mysql"
 )
@@ -31,55 +32,80 @@ func TestStatus_FindByID_Delete(t *testing.T) {
 	}
 }
 
-//func TestStatusAllStatuses(t *testing.T) {
-//	// set up
-//	dao := NewDao(t)
-//	defer dao.InitAll()
-//	ctx := context.Background()
-//	CreateBaseTable(t, dao)
+func TestStatusAllStatuses(t *testing.T) {
+	// set up
+	dao := NewDao(t)
+	defer dao.InitAll()
+	ctx := context.Background()
+	CreateBaseTable(t, dao)
 
-//	type args struct {
-//		ctx      context.Context
-//		since_id int64
-//		max_id   int64
-//		limit    int64
-//	}
-//	tests := []struct {
-//		name    string
-//		args    args
-//		want    []object.Status
-//		wantErr bool
-//	}{
-//		{
-//			name: "simple",
-//			args: args{
-//				ctx:      ctx,
-//				since_id: 0,
-//				max_id:   10,
-//				limit:    10,
-//			},
-//			want: []object.Status{
-//				{
-//					Content: "",
-//				},
-//			},
-//		},
-//	}
-//	for _, tt := range tests {
-//		tt := tt
-//		t.Run(tt.name, func(t *testing.T) {
-//			t.Parallel()
-//			r := &status{
-//				db: tt.fields.db,
-//			}
-//			got, err := r.AllStatuses(tt.args.ctx, tt.args.since_id, tt.args.max_id, tt.args.limit)
-//			if (err != nil) != tt.wantErr {
-//				t.Errorf("status.AllStatuses() error = %v, wantErr %v", err, tt.wantErr)
-//				return
-//			}
-//			if !reflect.DeepEqual(got, tt.want) {
-//				t.Errorf("status.AllStatuses() = %v, want %v", got, tt.want)
-//			}
-//		})
-//	}
-//}
+	type args struct {
+		ctx      context.Context
+		since_id int64
+		max_id   int64
+		limit    int64
+	}
+	tests := []struct {
+		name    string
+		args    args
+		want    []object.Status
+		wantErr bool
+	}{
+		{
+			name: "simple",
+			args: args{
+				ctx:      ctx,
+				since_id: 0,
+				max_id:   10,
+				limit:    10,
+			},
+			want: []object.Status{
+				{
+					Content: testStatusContent1,
+				},
+				{
+					Content: testStatusContent2,
+				},
+				{
+					Content: testStatusContent3,
+				},
+			},
+		},
+		{
+			name: "simple",
+			args: args{
+				ctx:      ctx,
+				since_id: 2,
+				max_id:   10,
+				limit:    10,
+			},
+			want: []object.Status{
+				{
+					Content: testStatusContent2,
+				},
+				{
+					Content: testStatusContent3,
+				},
+			},
+		},
+	}
+	for _, tt := range tests {
+		tt := tt
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := dao.Status().AllStatuses(tt.args.ctx, tt.args.since_id, tt.args.max_id, tt.args.limit)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("AllStatuses() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if len(got) == len(tt.want) {
+				for i, v := range got {
+					if v.Content != tt.want[i].Content {
+						t.Errorf("AllStatuses() = %v, want %v", got, tt.want)
+					}
+				}
+			} else {
+				t.Errorf("AllStatuses() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}

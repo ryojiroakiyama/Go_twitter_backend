dc24fc5a4bab722e7c3b0c04a5ce15658bc1f413
rakiyama <ryojiro25@gmail.com>
2022-05-12 23:55:32 +0900

    add: attachment object

---
+++ app/domain/object/attachment.go
@@ -0,0 +1,19 @@
+package object
+
+type (
+	AttachmentID = int64
+
+	Attachment struct {
+		// The ID of the attachment
+		ID AttachmentID `json:"id" db:"id"`
+
+		// The content_type of the attachment
+		Type string `json:"type,omitempty" db:"type"`
+
+		// Url(path) of the attachment
+		Url string `json:"url,omitempty" db:"url"`
+
+		// The time the attachment was created
+		Description *string `json:"description,omitempty" db:"description"`
+	}
+)
